6f54333af2e01ea29fdaee24c197adba899e9912
weslx <80607779+weslx@users.noreply.github.com>
2023-10-09 22:34:21 -0300

    Api add

--- pages/api/add.js
+++ pages/api/add.js
@@ -3,51 +3,61 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async function handle(req, res) {
-  const { datetime, productId, nameId, shelf, section, quantityBox, quantityItem } = req.body
+  try {
+    const { datetime, productId, nameId, shelf, section, quantityBox, quantityItem } = req.body
 
-  // Adicione validações aqui conforme necessário
-
-  const result = await prisma.$transaction([
-    prisma.box.create({
+    const box = await prisma.box.create({
       data: {
         datetime: new Date(datetime), // certifique-se de que datetime é uma string ISO 8601
       },
-    }),
-    prisma.product.create({
+    })
+
+    const product = await prisma.product.create({
       data: {
         productsName: {
-          connect: { id: Number(nameId) },
+          connectOrCreate: {
+            where: { id: Number(nameId) },
+            create: { name: "teste" },
+          },
         },
         box: {
-          connect: { id: Number(shelf) }, // assumindo que 'shelf' é o 'boxId'
+          connect: { id: box.id },
         },
         quantityBox: Number(quantityBox),
         quantityItem: Number(quantityItem),
       },
-    }),
-    prisma.stock.create({
+    })
+
+    const shelfsSections = await prisma.shelfsSections.create({
       data: {
-        boxId: Number(shelf), // assumindo que 'shelf' é o 'boxId'
-        productsId: Number(productId),
-        shelfsSectionsId: Number(section),
-      },
-    }),
-    prisma.shelfsSections.create({
-      data: {
-        shelf: 'Nome da Prateleira', // substitua pela variável apropriada
+        shelf: "t",
         section: Number(section),
       },
-    }),
-    prisma.remove.create({
-      data: {
-        productsId: Number(productId),
-        boxId: Number(shelf), // assumindo que 'shelf' é o 'boxId'
-        shelfsSectionsId: Number(section),
-        quantityBox: Number(quantityBox),
-        datetime: new Date(datetime),
-      },
-    }),
-  ])
+    })
 
-  res.json(result)
+    const result = await prisma.$transaction([
+      prisma.stock.create({
+        data: {
+          boxId: box.id,
+          productsId: product.id,
+          shelfsSectionsId: shelfsSections.id,
+        },
+      }),
+      prisma.remove.create({
+        data: {
+          productsId: product.id,
+          boxId: box.id,
+          shelfsSectionsId: shelfsSections.id,
+          quantityBox: Number(quantityBox),
+          datetime: new Date(datetime),
+        },
+      }),
+    ])
+
+    res.json(result)
+  } catch (error) {
+    console.error(error)
+  } finally {
+    await prisma.$disconnect()
+  }
 }

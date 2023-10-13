c0d32028f2d4f660ebbaba76111f9a56747e0bc7
weslx <80607779+weslx@users.noreply.github.com>
2023-10-12 23:32:57 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import Link from "next/link";
 
 const Index = () => (
-  <div>
+  <div className=" grid">
     <h1>Página Inicial</h1>
     <Link href="/home/home">
-      <button>Ir para o menu de adicionar/remover estoque</button>
+      <button>Ir para o menu de adicionar/remover item</button>
     </Link>
     <Link href="/find/find">
       <button>Ir para o menu de ver estoque</button>

d917446caedd237ce57244cd45f69fd805493409
weslx <wesleyaleixo0950@gmail.com>
2023-10-22 14:39:33 -0300

    css

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import { Analytics } from "@vercel/analytics/react";
 import "../output.css";
 import "styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import "../styles/find.css";
+import "../styles/style.css";
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/find.js
+++ pages/find.js
@@ -15,41 +15,50 @@ function ProductPage() {
   }, [id]);
 
   return (
-    <div className="container mx-auto p-4">
-      <input
-        type="text"
-        className="border-2 border-gray-300 p-2 rounded-md"
-        placeholder="Digite o id do produto"
-        value={id}
-        onChange={(e) => setId(e.target.value)}
-      />
-      <button
-        type="submit"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
-      >
-        Buscar
-      </button>
-      {product && ( // se o produto existir, mostra os dados dele
-        <div className="mt-4">
+    <div>
+      <header>Estoque</header>
+      <h1>Tabela</h1>
+      <div className="container mx-auto p-4">
+        <input
+          id="pesquisa"
+          type="text"
+          className="border-2 border-gray-300 p-2 rounded-md"
+          placeholder="Digite o id do produto"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+        <br></br>
+        <button
+          id="buscar"
+          type="submit"
+          className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
+        >
+          Buscar
+        </button>
+        {product && ( // se o produto existir, mostra os dados dele
           <div className="mt-4">
-            <h1 className="text-4xl font-bold">
-              Nome: {product.productsName.name}
-            </h1>
-            <p className="text-xl text-gray-600">
-              Este é um produto da caixa {product.boxId}
-            </p>
-            <ul className="list-disc list-inside">
-              <li>Quantidade de caixas: {product.quantityBox}</li>
-              <li>Quantidade de itens: {product.quantityItem}</li>
-              <li>Data da caixa: {product.box.datetime}</li>
-              <li>
-                Seção da prateleira: {product.stock[0].shelfsSections.shelf}-
-                {product.stock[0].shelfsSections.section}
-              </li>
-            </ul>
+            <div className="mt-4">
+              <h1 className="text-4xl font-bold"></h1>
+              <table class="tabela">
+                <tr>
+                  <th>ID</th>
+                  <th>Nome</th>
+                  <th>Quantidade</th>
+                  <th>Prateleira</th>
+                  <th>Seção</th>
+                </tr>
+                <tr>
+                  <td>{id}</td>
+                  <td>{product.productsName.name}</td>
+                  <td>{product.quantityItem}</td>
+                  <td>{product.stock[0].shelfsSections.shelf}</td>
+                  <td>{product.stock[0].shelfsSections.section}</td>
+                </tr>
+              </table>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }

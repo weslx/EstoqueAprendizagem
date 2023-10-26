b3120c66511fa1b0b14bb42e5f92e835bbe84848
jenny <jennyfernatalia559@gmail.com>
2023-10-26 13:58:26 -0300

    aooo wesli

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { Analytics } from "@vercel/analytics/react";
 import "../output.css";
 import "styles/globals.css";
+import "../styles/start.css"
 import "../styles/style.css";
 import "../styles/find.css";
 import { ClerkProvider } from "@clerk/nextjs";
--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,23 @@ import Link from "next/link";
 
 const Index = () => (
   <div className="grid grid-cols-1 gap-4 p-4">
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Caveat:wght@600&family=Inspiration&family=Kanit:wght@500&display=swap" rel="stylesheet"></link>
     <title>Homepage</title>
-    <h1 className="text-4xl font-bold text-center">Página Inicial</h1>
-    <p className="text-center font-bold">Sera necessario fazer login</p>
+    <h1 class='sttitle'>Página Inicial</h1>
+    <p class="stsub">Sera necessario fazer login</p>
+    <p class="stp">
     <Link href="/home">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
+    <button class='stb1'>
         Ir para o menu de adicionar/remover item
+        <img src="adcrmv.png"></img>
       </button>
     </Link>
     <Link href="/find">
-      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full">
+      <button class='stb2'>
         Ir para o menu de ver estoque
       </button>
     </Link>
+    </p>
   </div>
 );
 

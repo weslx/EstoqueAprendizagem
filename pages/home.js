0b0cd267c06936cd14b2d3ff034909cd609aaa6d
Leonardo Felix Apolinario <leofelixap34@gmail.com>
2023-11-08 14:33:53 -0300

    center

--- pages/home.js
+++ pages/home.js
@@ -94,50 +94,47 @@ export default function Home() {
       <div class="add">
         <h2 class="">Adicionar Item</h2>
         <form onSubmit={handleAddSubmit} class="addcontent">
-          <input
-            class="inpt1"
-            type="text"
-            placeholder="Nome"
-            name="name"
-            onChange={handleAddChange}
-          />
-          <input
-            class="inpt1"
-            type="text"
-            name="barcode"
-            placeholder="Codigo de barras"
-            onChange={handleAddChange}
-          />
-          <input
-            class="inpt1
+          <div class="inputs">
+            <input
+              class="inpt1"
+              type="text"
+              placeholder="Nome"
+              name="name"
+              onChange={handleAddChange}
+            />
+            <input
+              class="inpt1"
+              type="text"
+              name="barcode"
+              placeholder="Codigo de barras"
+              onChange={handleAddChange}
+            />
+            <input
+              class="inpt1
 "
-            type="text"
-            name="shelf"
-            placeholder="Prateleira"
-            onChange={handleAddChange}
-          />
-          <input
-            class="inpt1
+              type="text"
+              name="shelf"
+              placeholder="Prateleira"
+              onChange={handleAddChange}
+            />
+            <input
+              class="inpt1
 "
-            type="text"
-            placeholder="Seção"
-            name="section"
-            onChange={handleAddChange}
-          />
-          <input
-            class="inpt1
+              type="text"
+              placeholder="Seção"
+              name="section"
+              onChange={handleAddChange}
+            />
+            <input
+              class="inpt1
 "
-            type="text"
-            name="quantity_item"
-            placeholder="Quantidade do Item"
-            onChange={handleAddChange}
-          />
-          <button
-            id="bt1"
-            class="w-full py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600"
-          >
-            Adicionar
-          </button>
+              type="text"
+              name="quantity_item"
+              placeholder="Quantidade do Item"
+              onChange={handleAddChange}
+            />
+            <button id="bt1">Adicionar</button>
+          </div>
         </form>
       </div>
       <a href="table.html">
@@ -146,19 +143,21 @@ export default function Home() {
       <div class="rmv">
         <h2 class="">Remover Item</h2>
         <form onSubmit={handleRemoveSubmit} class="addcontent">
-          <input
-            class="inpt2"
-            type="text"
-            placeholder="ID"
-            name="id"
-            onChange={handleRemoveChange}
-          />
-          <button
-            id="bt2"
-            class="w-full py-2 px-4 bg-red-500 text-white rounded hover:bg-red-600"
-          >
-            Remover
-          </button>
+          <div class="inputs">
+            <input
+              class="inpt2"
+              type="text"
+              placeholder="ID"
+              name="id"
+              onChange={handleRemoveChange}
+            />
+            <button
+              id="bt2"
+              class="w-full py-2 px-4 bg-red-500 text-white rounded hover:bg-red-600"
+            >
+              Remover
+            </button>
+          </div>
         </form>
       </div>
     </div>
